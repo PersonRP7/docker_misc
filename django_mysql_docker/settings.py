207adb97059b8dfaa200aff6d39268e8c2f04a10
Person <zemlja-trp@yandex.com>
2020-12-26 09:25:24 +0100

    Added settings.py

---
+++ django_mysql_docker/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'some_database',
+        'USER': 'root',
+        'PASSWORD': 'some_password',
+        'HOST': 'db',
+        'OPTIONS': {'charset': 'utf8mb4'},
+    }
+}
